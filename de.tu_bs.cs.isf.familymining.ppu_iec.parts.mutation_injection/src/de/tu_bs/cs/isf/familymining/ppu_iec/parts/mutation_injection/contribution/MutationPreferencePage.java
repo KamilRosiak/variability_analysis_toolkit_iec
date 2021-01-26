559e17b64088509b9dc25a9cbb5df9c3b7cd9971
Kamil Rosiak <k.rosiak@tu-bs.de>
2021-01-26 13:08:06 +0100

    removed fxml

--- de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection/src/de/tu_bs/cs/isf/familymining/ppu_iec/parts/mutation_injection/contribution/MutationPreferencePage.java
+++ de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection/src/de/tu_bs/cs/isf/familymining/ppu_iec/parts/mutation_injection/contribution/MutationPreferencePage.java
@@ -1,16 +1,26 @@
 package de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection.contribution;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
 
 import de.tu_bs.cs.isf.e4cf.core.preferences.interfaces.IPreferencePage;
 import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
 
 public class MutationPreferencePage implements IPreferencePage {
-	
+    private static final String MUATION_PREF_PAGE_FXML = "/view/prefPage.fxml";
 	@Override
 	public void createPage(CTabFolder parent, ServiceContainer services) {
-		// TODO Auto-generated method stub
-		
+		CTabItem tab = new CTabItem(parent, SWT.NONE);
+		tab.setText("Mutation");
+		Composite page = new Composite(parent,SWT.None);
+		page.setLayout(new FillLayout(SWT.VERTICAL));
+	
+
+        
+        tab.setControl(page);
 	}
 
 	@Override
--- de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection/src/de/tu_bs/cs/isf/familymining/ppu_iec/parts/mutation_injection/scenarios/MutationST.java
+++ de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection/src/de/tu_bs/cs/isf/familymining/ppu_iec/parts/mutation_injection/scenarios/MutationST.java
@@ -1,13 +1,12 @@
 package de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection.scenarios;
 
-
 /**
- * StringTable for the mutation bundle 
+ * StringTable for the mutation bundle
+ * 
  * @author Kamil Rosiak
  *
  */
 public class MutationST {
-
 	public static final String BUNDLE_NAME = "de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection";
 	public static final String NUMBER_RUNS_PREF = "NUMBER_RUNS";
 }
