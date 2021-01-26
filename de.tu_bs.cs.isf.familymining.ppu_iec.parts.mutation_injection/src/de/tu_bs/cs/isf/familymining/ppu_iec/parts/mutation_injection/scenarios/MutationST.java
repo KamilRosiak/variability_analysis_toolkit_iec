d8b8f9afe371e751c84372df6273999fc5a5711c
Kamil Rosiak <k.rosiak@tu-bs.de>
2021-01-26 12:38:02 +0100

    added java doc

--- de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection/src/de/tu_bs/cs/isf/familymining/ppu_iec/parts/mutation_injection/scenarios/MutationST.java
+++ de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection/src/de/tu_bs/cs/isf/familymining/ppu_iec/parts/mutation_injection/scenarios/MutationST.java
@@ -1,7 +1,13 @@
 package de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection.scenarios;
 
+
+/**
+ * StringTable for the mutation bundle 
+ * @author Kamil Rosiak
+ *
+ */
 public class MutationST {
-	
+
 	public static final String BUNDLE_NAME = "de.tu_bs.cs.isf.familymining.ppu_iec.parts.mutation_injection";
 	public static final String NUMBER_RUNS_PREF = "NUMBER_RUNS";
 }
